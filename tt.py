7f901779807823afd399a594b38e2190ae6dbbc4
aashnajoshi <aashnahetal@gmail.com>
2024-04-03 11:26:55 +0530

    .

--- tt.py
+++ tt.py
@@ -1,4 +1,18 @@
-from tt_struct import create_timetable, tt_structure
+from openpyxl import Workbook
+
+tt_structure = [["", "1st", "2nd", "3rd", "4th","", "5th", "6th", "7th"], ["Mon"], ["Tues"], ["Wed"], ["Thurs"], ["Fri"]]
+
+def create_timetable(x, tt_structure):
+    wb = Workbook()
+    ws = wb.active
+    for row in tt_structure:
+        ws.append(row)
+
+    ws.merge_cells('F1:F6')
+    ws.merge_cells('H4:I4')
+# to add a condition that if there is a lab lec then 2 cells must be merged using above syntax.
+    wb.save(f"{x}_TimeTable.xlsx")
+    print(f"{x}_TimeTable.xlsx created!")
 
 def get_initial_info():
     # fetch from db: no. names of teachers
@@ -11,7 +25,7 @@ def get_initial_info():
         num_of_subjects = int(input(f"Number of Subjects taught by {name}: "))
         subjects = [input(f"Subject {j+1} for {name}: ") for j in range(num_of_subjects)]
         teachers[name] = subjects
-    return teachers
+    return teachers, subjects
 
 teachers_info = get_initial_info()
 for name in teachers_info.keys():
