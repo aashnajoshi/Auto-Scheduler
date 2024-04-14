c680b2d1dd372c469811616f38e13a172e5ff5cd
aashnajoshi <aashnahetal@gmail.com>
2024-04-14 16:38:02 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QAction, QMenuBar, QFileDialog, QTabWidget, QVBoxLayout, QWidget, QHBoxLayout, QTableWidget, QTableWidgetItem, QHeaderView, QMessageBox
+from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 import warnings
 
@@ -9,6 +10,7 @@ class TimeTableGenerator(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.new_window_size = self.calculateNewWindowSize()
 
     def initUI(self):
         self.setFixedSize(716, 553)
@@ -59,7 +61,12 @@ class TimeTableGenerator(QMainWindow):
         if title != "Scenario Manager":
             add_button_layout = QHBoxLayout()
             add_button = QPushButton(f"Add {title[:-1]}", self)
-            add_button.clicked.connect(lambda: self.addButtonClicked(title))
+            if title == "Instructors":
+                add_button.clicked.connect(self.addInstructorButtonClicked)
+            elif title == "Subjects":
+                add_button.clicked.connect(self.addSubjectButtonClicked)
+            else:
+                add_button.clicked.connect(lambda: self.addButtonClicked(title))
             add_button_layout.addWidget(add_button)
 
             import_button = QPushButton("Import from CSV", self)
@@ -106,14 +113,90 @@ class TimeTableGenerator(QMainWindow):
         print("About")
 
     def addButtonClicked(self, title):
+        self.new_window = QMainWindow()
+        self.new_window.setWindowTitle(title)
+        self.new_window.setFixedSize(self.new_window_size)
+        print(f"Add {title} Clicked")
+
+    def calculateNewWindowSize(self):
         main_window_size = self.size()
         new_window_width = main_window_size.width() * 0.8
         new_window_height = main_window_size.height() * 0.8
+        return QtCore.QSize(new_window_width, new_window_height)
 
-        self.new_window = QMainWindow()
-        self.new_window.setWindowTitle(title)
-        self.new_window.setGeometry(self.geometry().center().x() - new_window_width / 2, self.geometry().center().y() - new_window_height / 2, new_window_width, new_window_height)
-        self.new_window.show()
+    def addInstructorButtonClicked(self):
+        instructor_dialog = QDialog(self)
+        instructor_dialog.setFixedSize(self.new_window_size)
+        instructor_dialog.setWindowTitle("Instructor")
+        
+        layout = QVBoxLayout()
+        
+        form_layout = QFormLayout()
+        self.lineEditName = QLineEdit()
+        self.lineEditHours = QLineEdit()
+        form_layout.addRow("Name", self.lineEditName)
+        form_layout.addRow("Available Hours", self.lineEditHours)
+        layout.addLayout(form_layout)
+        
+        self.tableSchedule = QTableView()
+        layout.addWidget(self.tableSchedule)
+        
+        button_layout = QHBoxLayout()
+        self.btnFinish = QPushButton("Finish")
+        self.btnCancel = QPushButton("Cancel")
+        self.btnCancel.clicked.connect(instructor_dialog.close)  # Connect cancel button to close window
+        button_layout.addWidget(self.btnFinish)
+        button_layout.addWidget(self.btnCancel)
+        layout.addLayout(button_layout)
+        
+        instructor_dialog.setLayout(layout)
+        instructor_dialog.exec_()
+
+    def addSubjectButtonClicked(self):
+        subject_dialog = QDialog(self)
+        subject_dialog.setFixedSize(self.new_window_size)
+        subject_dialog.setWindowTitle("Subject")
+
+        layout = QVBoxLayout()
+
+        grid_layout = QGridLayout()
+        self.lineEditName = QLineEdit()
+        self.lineEditHours = QLineEdit()
+        self.lineEditCode = QLineEdit()
+        self.lineEditDescription = QLineEdit()
+
+        grid_layout.addWidget(QLabel("Name:"), 0, 0)
+        grid_layout.addWidget(self.lineEditName, 0, 1)
+        grid_layout.addWidget(QLabel("Hours/Week:"), 0, 2)
+        grid_layout.addWidget(self.lineEditHours, 0, 3)
+        grid_layout.addWidget(QLabel("Code:"), 1, 0)
+        grid_layout.addWidget(self.lineEditCode, 1, 1)
+        grid_layout.addWidget(QLabel("Description:"), 1, 2)
+        grid_layout.addWidget(self.lineEditDescription, 1, 3)
+
+        layout.addLayout(grid_layout)
+
+        group_type = QGroupBox("Type")
+        group_type_layout = QHBoxLayout()
+        self.radioLec = QRadioButton("Lecture")
+        group_type_layout.addWidget(self.radioLec)
+        self.radioLab = QRadioButton("Laboratory")
+        group_type_layout.addWidget(self.radioLab)
+        self.radioBoth = QRadioButton("Both")
+        group_type_layout.addWidget(self.radioBoth)
+        group_type.setLayout(group_type_layout)
+        layout.addWidget(group_type)
+
+        button_layout = QHBoxLayout()
+        self.btnFinish = QPushButton("Finish")
+        button_layout.addWidget(self.btnFinish)
+        self.btnCancel = QPushButton("Cancel")
+        button_layout.addWidget(self.btnCancel)
+        self.btnCancel.clicked.connect(subject_dialog.close)
+        layout.addLayout(button_layout)
+
+        subject_dialog.setLayout(layout)
+        subject_dialog.exec_()
 
     def importButtonClicked(self):
         filename, _ = QFileDialog.getOpenFileName(self, "Import CSV", "", "CSV Files (*.csv)")
